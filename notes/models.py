5f45751c3ddc165af27f01a514f4d358f89ff1a5
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-05-05 18:47:52 +0100

    Adjusted body margin

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.CharField(max_length=200)
+    is_archived = models.BooleanField()
+    color = models.CharField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
