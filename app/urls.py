d787425cf3ebe3b08e10efdb02c3d873aa28a9ae
brunomartins-rdev <bruno.silva.martins@hotmail.com>
2025-05-12 17:11:30 +0100

    Template for notes working

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+        path('', include('notes.urls')),
         path('notes/', include('notes.urls')),
         path('admin/', admin.site.urls),
         ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from .models import Note
+
 # Create your views here.
 def index(request):
-    return render(request, 'notes.html')
+    notes_list = Note.objects.order_by('-created_at')
+    print(notes_list)
+    context = {'notes_list': notes_list}
+    return render(request, 'notes.html', context)
 
